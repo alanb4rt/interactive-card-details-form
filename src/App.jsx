24c1d3ca476bc927321b68c60df0a99eb039e842
alanb4rt <al4n.b4rt@gmail.com>
2024-08-22 13:51:42 +0200

    create PostSubmissionConfirmation component

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,37 @@
 import { useState } from "react";
 import Card from "./components/Card";
 import Form from "./components/Form";
+import PostSubmissionConfirmation from "./components/PostSubmissionConfirmation";
 
 export default function App() {
   const [formData, setFormData] = useState({});
+  const [isFormCompleted, setIsFormCompleted] = useState(false);
 
   const handleFormDataChange = (newData) => {
     setFormData(newData);
   };
+
+  const toggleFormCompletion = () => {
+    setIsFormCompleted(!isFormCompleted);
+  };
+
   return (
     <>
       <div className="container mx-auto min-h-screen flex flex-col justify-center">
         <div className="grid grid-cols-2 items-center gap-16">
           <Card formData={formData} />
-          <Form handleFormDataChange={handleFormDataChange} />
+          <section className="w-full h-full flex flex-col justify-center items-center">
+            <div className="w-full max-w-xs">
+              {!isFormCompleted ? (
+                <Form handleFormDataChange={handleFormDataChange} />
+              ) : (
+                <PostSubmissionConfirmation />
+              )}
+              <button onClick={toggleFormCompletion}>
+                toggle (remove later)
+              </button>
+            </div>
+          </section>
         </div>
       </div>
     </>
---
+++ src/components/PostSubmissionConfirmation.jsx
@@ -0,0 +1,18 @@
+import IconComplete from "../assets/images/icon-complete.svg";
+
+export default function PostSubmissionConfirmation() {
+  return (
+    <>
+      <div className="w-full flex flex-col items-center gap-4">
+        <img className="size-20 mb-6" src={IconComplete} alt="" />
+        <h2 className="text-2xl font-bold uppercase tracking-widest">
+          Thank you!
+        </h2>
+        <p className="text-[var(--dark-grayish-violet)]">
+          We've added your card details
+        </p>
+        <button className="mt-6">Continue</button>
+      </div>
+    </>
+  );
+}
